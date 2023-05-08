d3c393a5f02e76785599ee4331cd00dc878cd1c8
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-08 20:25:48 +0200

    Create model Artist with properties

---
+++ Howest.Magic.DAL/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class Artist
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
