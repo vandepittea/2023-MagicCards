ede28dec4c766550ef89b942e9742c5957df7b38
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-08 20:26:38 +0200

    Create model CardColor with properties and collections for colors and types

---
+++ Howest.Magic.DAL/Models/CardColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class CardColor
+    {
+        public long CardId { get; set; }
+        public long ColorId { get; set; }
+
+        public virtual Card Card { get; set; }
+        public virtual Color Color { get; set; }
+    }
+}
