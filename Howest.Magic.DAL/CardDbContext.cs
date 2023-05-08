ae42ba518c7cbb4b51c96f20990f69ea290aa88a
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-08 21:51:17 +0200

    Create CardDbContext class with dbsets for each model and an empty onModelCreating method

---
+++ Howest.Magic.DAL/CardDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Howest.MagicCards.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL
+{
+    public class CardDbContext : DbContext
+    {
+        public CardDbContext(DbContextOptions<CardDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<CardColor> CardColors { get; set; }
+        public DbSet<CardType> CardTypes { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Rarity> Rarities { get; set; }
+        public DbSet<Set> Sets { get; set; }
+        public DbSet<Models.Type> Types { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
