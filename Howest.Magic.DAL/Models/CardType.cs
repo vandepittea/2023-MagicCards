64fda61abf37272551e3d022632184a00ee31419
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-08 20:28:21 +0200

    Create model CardType with properties and collections for card and type

---
+++ Howest.Magic.DAL/Models/CardType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class CardType
+    {
+        public long CardId { get; set; }
+        public long TypeId { get; set; }
+
+        public virtual Card Card { get; set; }
+        public virtual Type Type { get; set; }
+    }
+}
