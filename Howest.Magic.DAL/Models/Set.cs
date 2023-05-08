aab028cd3453961cd1d727be4a256016861f5dba
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-08 20:30:45 +0200

    Create model Set with properties and collection for cards

---
+++ Howest.Magic.DAL/Models/Set.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class Set
+    {
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Card> Cards { get; set; } = new List<Card>();
+    }
+}
