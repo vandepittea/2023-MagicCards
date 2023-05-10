9e691872be4125e2b4b7b59d592bbf5d6bd00565
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-10 17:44:24 +0200

    Add CardsController with action to get cards and already a preparation for paging

---
+++ Howest.Magic.DAL/Repositories/CardRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories
+{
+    public class CardRepository
+    {
+    }
+}
---
+++ Howest.Magic.WebAPI/Controllers/CardsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Howest.MagicCards.DAL.Repositories;
+using Howest.MagicCards.DAL.Models;
+
+namespace Howest.MagicCards.WebAPI.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly CardRepository _cardRepository;
+
+        public CardsController(ICardRepository cardRepository)
+        {
+            _cardRepository = new CardRepository();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCards(
+            [FromQuery] string setName,
+            [FromQuery] string artistName,
+            [FromQuery] string rarity,
+            [FromQuery] string type,
+            [FromQuery] string searchQuery,
+            [FromQuery] string sortBy,
+            [FromQuery] bool sortAscending = true,
+            [FromQuery] int page = 1,
+            [FromQuery] int pageSize = 50)
+        {
+            var cards = await _cardRepository.GetFilteredAndSortedCards(setName, artistName, rarity, type, searchQuery, sortBy, sortAscending, page, pageSize);
+            var totalCards = cards.Count();
+            var pagedCards = cards.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            var pagedResponse = new PagedResponse<IEnumerable<Card>>(pagedCards, page, pageSize, totalCards);
+            return Ok(pagedResponse);
+        }
+    }
+}
