2ba46fa935dcec1aec18ebba5a7240168007c4c4
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-10 17:04:51 +0200

    Optimize code

--- Howest.Magic.DAL/CardDbContext.cs
+++ Howest.Magic.DAL/CardDbContext.cs
@@ -34,7 +34,7 @@ public partial class CardDbContext : DbContext
 
     public virtual DbSet<Set> Sets { get; set; }
 
-    public virtual DbSet<Type> Types { get; set; }
+    public virtual DbSet<Models.Type> Types { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
@@ -328,7 +328,7 @@ public partial class CardDbContext : DbContext
                 .HasColumnName("updated_at");
         });
 
-        modelBuilder.Entity<Type>(entity =>
+        modelBuilder.Entity<Models.Type>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__types__3213E83F62126D10");
 
