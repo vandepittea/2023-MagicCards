bcf85ea74d60ddd2c5b57f3ec69805abcc67ce42
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-12 00:57:58 +0200

    Rethink PagedResponse

---
+++ Howest.Magic.WebAPI/Wrappers/PagedResponse.cs
@@ -0,0 +1,27 @@
+﻿using Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class PagedResponse<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+        public T Data { get; set; }
+
+        public PagedResponse(T data, int pageNumber, int pageSize, int totalRecords)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            Data = data;
+            TotalRecords = totalRecords;
+            TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+        }
+    }
+}
