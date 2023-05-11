a87c801439fd5cecb4e562b873f540a52fde68f5
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-12 00:22:15 +0200

    After redoing dtos, also redo CardProfile

---
+++ Howest.MagicCards.Shared/Mappings/CardProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.Shared
+{
+    public class CardProfile : Profile
+    {
+        public CardProfile()
+        {
+            CreateMap<Card, CardDto>()
+                .ForMember(dto => dto.CardColors,
+                           opt => opt.MapFrom(c => c.CardColors.Select(cc => cc.Color.Code)))
+                .ForMember(dto => dto.CardTypes,
+                           opt => opt.MapFrom(c => c.CardTypes.Select(ct => ct.Type)))
+                .ReverseMap();
+
+            CreateMap<Card, CardDetailDto>()
+                .IncludeBase<Card, CardDto>()
+                .ForMember(dto => dto.ArtistName,
+                           opt => opt.MapFrom(c => c.Artist.FullName))
+                .ReverseMap();
+        }
+    }
+}
