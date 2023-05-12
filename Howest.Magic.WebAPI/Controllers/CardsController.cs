ac8b27e79a229cc78d228a5564dbbd4607787adb
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-12 19:12:12 +0200

    Add versioning and swagger

--- Howest.Magic.WebAPI/Controllers/CardsController.cs
+++ Howest.Magic.WebAPI/Controllers/CardsController.cs
@@ -11,8 +11,9 @@ using Howest.MagicCards.Shared.Extensions;
 using AutoMapper.QueryableExtensions;
 
 
-namespace Howest.MagicCards.WebAPI.Controllers
+namespace Howest.MagicCards.WebAPI.Controllers.V1
 {
+    [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class CardsController : ControllerBase
@@ -27,6 +28,78 @@ namespace Howest.MagicCards.WebAPI.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(CardDto), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        [ProducesResponseType(typeof(string), 500)]
+        public async Task<ActionResult<PagedResponse<IQueryable<CardDto>>>> GetCards([FromQuery] CardFilter filter,
+            [FromQuery] bool sortOrder,
+            [FromServices] IConfiguration config)
+        {
+            try
+            {
+                filter.MaxPageSize = int.Parse(config["maxPageSize"]);
+
+                IQueryable<Card> allCards = (IQueryable<Card>)await _cardRepository.GetCards();
+
+                if (allCards == null)
+                {
+                    return NotFound(new Response<CardDto>()
+                    {
+                        Errors = new string[] { "404" },
+                        Message = "No cards found"
+                    });
+                }
+
+                IQueryable<Card> filteredCards = allCards
+                    .FilterBySet(filter.SetCode)
+                    .FilterByArtist(filter.ArtistName)
+                    .FilterByRarity(filter.RarityCode)
+                    .FilterByCardType(filter.CardType)
+                    .FilterByCardName(filter.CardName)
+                    .FilterByCardText(filter.CardText);
+
+                IEnumerable<CardDto> pagedCards = filteredCards
+                    .ToPagedList<Card>(filter.PageNumber, filter.PageSize)
+                    .ProjectTo<CardDto>(_mapper.ConfigurationProvider)
+                    .ToList();
+
+                int totalCount = filteredCards.Count();
+                int totalPages = (int)Math.Ceiling(totalCount / (double)filter.PageSize);
+
+                return Ok(new PagedResponse<CardDto>(pagedCards, filter.PageNumber, filter.PageSize, totalCount, totalPages));
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response<CardDto>()
+                    {
+                        Succeeded = false,
+                        Errors = new string[] { $"Status code: {StatusCodes.Status500InternalServerError}" },
+                        Message = $"({ex.Message}) "
+                    });
+            }
+        }
+    }
+}
+
+namespace Howest.MagicCards.WebAPI.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly CardRepository _cardRepository;
+        private readonly IMapper _mapper;
+
+        public CardsController(CardRepository cardRepository, IMapper mapper)
+        {
+            _cardRepository = cardRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(CardDto), 200)]
         [ProducesResponseType(typeof(string), 404)]
         [ProducesResponseType(typeof(string), 500)]
         public async Task<ActionResult<PagedResponse<IQueryable<CardDto>>>> GetCards([FromQuery] CardFilter filter,
@@ -67,7 +140,7 @@ namespace Howest.MagicCards.WebAPI.Controllers
 
                 return Ok(new PagedResponse<CardDto>(pagedCards, filter.PageNumber, filter.PageSize, totalCount, totalPages));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError,
                     new Response<CardDto>()
--- Howest.Magic.WebAPI/Program.cs
+++ Howest.Magic.WebAPI/Program.cs
@@ -6,6 +6,8 @@ using Howest.MagicCards.Shared.DTOs;
 using Howest.MagicCards.Shared.Validation;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 ConfigurationManager config = builder.Configuration;
@@ -18,6 +20,22 @@ builder.Services.AddControllers()
     });
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MTG API version 1",
+        Version = "v1",
+        Description = "API to manage cards"
+    });
+    c.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Title = "MTG API version 2",
+        Version = "v2",
+        Description = "API to manage cards"
+    });
+});
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<MtgDbContext>
@@ -30,14 +48,38 @@ builder.Services.AddScoped<IValidator<CardDto>, CardValidator>();
 
 builder.Services.AddAutoMapper(typeof(Howest.MagicCards.Shared.Mappings.CardProfile));
 
+builder.Services.AddApiVersioning(o =>
+{
+    o.ReportApiVersions = true;
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.DefaultApiVersion = new ApiVersion(1, 0);
+});
+builder.Services.AddVersionedApiExplorer(
+    options =>
+    {
+        // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+        // note: the specified format code will format the version as "'v'major[.minor][-status]"
+        options.GroupNameFormat = "'v'VVV";
+
+        // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+        // can also be used to control the format of the API version in route templates
+        options.SubstituteApiVersionInUrl = true;
+    }
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MTG API v1");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "MTG API v2");
+    });
 }
+
 
 app.UseHttpsRedirection();
 
